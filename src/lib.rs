69f75fcb30442a600a88333ef7d9e467a6d24371
hcasten <huntercasten@icloud.com>
2024-07-10 00:28:55 -0600

    feat(fuzzy): setup test framework and the basic scoring and matching interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,90 @@ use std::str::Chars;
 
 use itertools::Itertools;
 
+pub trait ScoringAlgorithm {
+    fn score(&self, search: &str, matching_characters: &str) -> usize;
+}
+
+trait SortAndFilter<T, F> {
+    fn sort_and_filter(&mut self, scorer: &F, threshold: usize) -> Vec<T>;
+}
+
+impl<'a, F, T: Iterator<Item = Match<'a>>> SortAndFilter<&'a str, F> for T
+where
+    F: ScoringAlgorithm,
+{
+    fn sort_and_filter(&mut self, scorer: &F, threshold: usize) -> Vec<&'a str> {
+        let mut map = HashMap::new();
+        // for item in self.iter() {
+        while let Some(item) = self.next() {
+            let score = item.score(scorer);
+            if score >= threshold {
+                map.insert(score, item.result);
+            }
+        }
+
+        map.into_iter()
+            .sorted_by(|(a, _), (b, _)| b.cmp(a))
+            .map(|(_, v)| v)
+            .collect()
+    }
+}
+
+struct SlightlyMoreSophisticatedScorer;
+impl ScoringAlgorithm for SlightlyMoreSophisticatedScorer {
+    fn score(&self, search: &str, matching_characters: &str) -> usize {
+        let mut score = 0;
+        let mut search = search.chars();
+        let mut matching_characters = matching_characters.chars();
+        while let (Some(s), Some(m)) = (search.next(), matching_characters.next()) {
+            if s == m {
+                score += 1;
+            }
+        }
+        score
+    }
+
+    // add code here
+}
+struct SimpleScorer;
+impl ScoringAlgorithm for SimpleScorer {
+    fn score(&self, search: &str, matching_characters: &str) -> usize {
+        if search.is_empty() {
+            return 0;
+        }
+        if matching_characters.is_empty() {
+            return 0;
+        }
+        0
+    }
+}
+
+pub struct Match<'a> {
+    search: String,
+    matching_characters: Vec<char>,
+    result: &'a str,
+}
+
+impl<'a> Match<'a> {
+    pub fn new(search: String, matching_characters: Vec<char>, result: &'a str) -> Self {
+        Self {
+            search,
+            matching_characters,
+            result,
+        }
+    }
+
+    pub fn score<F>(&self, scorer: &F) -> usize
+    where
+        F: ScoringAlgorithm,
+    {
+        scorer.score(
+            &self.search,
+            self.matching_characters.iter().collect::<String>().as_str(),
+        )
+    }
+}
+
 #[derive(Default, Debug)]
 pub struct TrieNode {
     children: HashMap<char, TrieNode>,
@@ -52,8 +136,6 @@ impl Default for SearchOptions {
         }
     }
 }
-
-struct Match {}
 
 impl TrieNode {
     pub fn insert(&mut self, value: &str) {
@@ -111,6 +193,8 @@ mod tests {
 
     use itertools::assert_equal;
 
+    use crate::old::Score;
+
     use super::*;
 
     #[test]
@@ -150,5 +234,46 @@ mod tests {
         let actual = trie.search("test");
         dbg!(&actual);
         assert_equal(expected, actual)
+    }
+
+    #[test]
+    fn test_match_scoring() {
+        let result = Match::new(
+            "test".to_string(),
+            vec!['t', 'e', 's', 't'],
+            "I don't think I'll pass the science test",
+        );
+        let scorer = SlightlyMoreSophisticatedScorer;
+
+        let score = result.score(&scorer);
+
+        assert_eq!(4, score);
+        // assert!(score == 4.0);
+    }
+
+    #[test]
+    fn sorting_scores() {
+        let results = vec![
+            Match::new(
+                "test".to_string(),
+                vec!['t', 'e', 's', 't'],
+                "I don't think I'll pass the science test",
+            ),
+            Match::new(
+                "test".to_string(),
+                vec!['t', 'e', 's', 't'],
+                "This is a test",
+            ),
+        ];
+
+        // let mut iter = results.iter();
+        let result = results
+            .into_iter()
+            .sort_and_filter(&SlightlyMoreSophisticatedScorer, 4);
+
+        assert_equal(
+            vec!["This is a test", "I don't think I'll pass the science test"],
+            result,
+        )
     }
 }
